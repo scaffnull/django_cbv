70b954cd4bcf02f03fa55e7f3b534b2747d14f92
Mathias Åhman <mathiasahman2@gmail.com>
2024-01-18 10:25:00 +0100

    added views

--- basic_app/views.py
+++ basic_app/views.py
@@ -10,10 +10,11 @@ from django.views.generic import View, TemplateView
 #     def get(self,request):
 #         return HttpResponse("Classed based views are cool")
 
+# TEMPLATE VIEW
 class IndexView(TemplateView):
     template_name = 'basic_app/index.html'
 
-    def get_context_date(self,**kwargs):
+    def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'BASIC INJECTION!'
         return context
