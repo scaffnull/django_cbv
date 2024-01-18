d1e5d241ef1d250351e5b9c7452c67499777dbb2
Mathias Åhman <mathiasahman2@gmail.com>
2024-01-18 10:24:38 +0100

    add admin and models

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    principal = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.PositiveIntegerField()
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
